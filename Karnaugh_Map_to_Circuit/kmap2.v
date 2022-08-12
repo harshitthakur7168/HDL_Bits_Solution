f48fce99a3574b6b7d181085949bae64c3c168bd
Harshit Thakur <96690206+harshitthakur7168@users.noreply.github.com>
2022-08-12 12:05:43 +0530

    Create kmap2.v

---
+++ Karnaugh_Map_to_Circuit/kmap2.v
@@ -0,0 +1,21 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  );
+    wire a_no, b_no, c_no, d_no, w1,w2,w3,w4;
+    not ( a_no, a);
+    not ( b_no, b);
+    not ( c_no, c);
+    not ( d_no, d);
+    
+    and ( w1, d_no, a_no);
+    and (w2, b_no, c_no);
+    and (w3, a,c,d);
+    and (w4, b,c,d);
+    
+    or ( out, w1,w2,w3,w4);
+    
+
+endmodule
